c47b643c5b3a414a28185bfda49d2c9ab7629b2c
Davide Vitali <davide.bass@gmail.com>
2021-04-30 12:45:31 +0200

    aggiunto argParse

--- local.js
+++ local.js
@@ -1,47 +1,15 @@
-const BotK = require('./src/botk.js')
+const BotK = require('./src/botk.js');
+const ArgParser = require('./src/_textModules/argParser.js');
 
 // bool finale che decide se il comando digitato è valido
 var validEntry = true;
 
-// elimina node.exe e il nome del file
-var inputArgs = process.argv.slice(2);
+var argParser = new ArgParser(process.argv.slice(2), 'local');
 
-// oggetto che memorizza il comando e le opzioni coi loro valori
-var botCommandLine = new Object();
-botCommandLine.command = inputArgs[0];
-
-var optionArgs = [];
-
-// array che esamina solo le opzioni
-if (inputArgs.length > 0) {
-    optionArgs = inputArgs.slice(1);
-} else {
-    optionArgs = inputArgs;
-}
-
-if (botCommandLine.command && botCommandLine.command.toUpperCase() == 'BK')
-{
-    console.log('optionArgs:', optionArgs);
-    for (let i = 0; i < optionArgs.length; i++)  {
-        if (optionArgs[i].substring(0, 1) !== '-') {
-            botCommandLine.error = (String(optionArgs[i]) + " non è un'opzione valida.");
-            validEntry = false;
-        } else {
-            var option = optionArgs[i].substring(1).split(':');
-            if (!option[1]) {
-                botCommandLine[option[0]] = '';
-            } else {
-                botCommandLine[option[0]] = option[1];
-            }
-        }
-        if (validEntry == false) { break; }
-    }
-    
-    if (validEntry == true) {
-        console.log('botCommandLine:', botCommandLine);
-        const bot = new BotK(botCommandLine);
-        bot.Exec()
-        .then(result => console.log(result))
-        .catch(error => console.log(error));
-    }
+if (argParser.isValid == true) {
+    console.log('da local.js: ', argParser.commandResult);
+    const bot = new BotK(botCommandLine);
+    bot.Exec()
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
 }
---
+++ src/_textModules/argParser.js
@@ -0,0 +1,53 @@
+/* -------------------------------
+    DA LOCAL
+    ----------------------------*/
+
+class ArgParser {
+    // inputArgs è un array e corrisponde a process.argv nel caso di chiamata da console, oppure da message.content.split(' ') nel caso di chiamata da discord
+    /**
+     * 
+     * @param {Array<String>} inputArgs - l'array di comando e opzioni digitati dall'utente 
+     * @param {['local', 'index']} src - la sorgente del comando: 'local' per le chiamate da console, 'index' per le chiamate tramite discord.
+     */
+    constructor(inputArgs, src){
+        this.commandResult = new Object();
+        this.isValid = false;
+
+        // oggetto che memorizza il comando e le opzioni coi loro valori
+        this.commandResult.command = inputArgs[0];
+        
+        if (this.commandResult.command && this.commandResult.command.toUpperCase() == 'BK')
+        {
+            var optionArgs = [];
+        
+            // array che esamina solo le opzioni
+            if (inputArgs.length > 0) {
+                optionArgs = inputArgs.slice(1);
+            } else {
+                optionArgs = inputArgs;
+            }
+
+            for (let i = 0; i < optionArgs.length; i++)  {
+                if (optionArgs[i].includes('=')) {
+                    var option = optionArgs[i].split('=');
+    
+                    if (!option[1]) {
+                        this.commandResult[option[0]] = '';
+                    } else {
+                        this.commandResult[option[0]] = option[1];
+                    }
+                    this.isValid = true;
+                }
+                
+                if (this.isValid == false) { 
+                    break;
+                }
+
+                // si prepara all'iterazione successiva
+                this.isValid = false;
+            }
+        }
+    }
+}
+
+module.exports = ArgParser;
