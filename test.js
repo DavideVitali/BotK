3fd86c608f81192c44fca48eafc2960d80b50b13
Davide Vitali <>
2021-05-14 08:48:19 +0000

    update

--- test.js
+++ test.js
@@ -10,26 +10,38 @@ const Swapi = require('./src/api/swgohApi.js')
 const Jimp = require('jimp');
 const swapi = new Swapi();
 const fetch = require('node-fetch');
+const statCalculator = require('swgoh-stat-calc');
 
 /**
  * param {Array<string>} members - Array dei codici alleato dei membri della Gilda.
  */
-function guildTeamStats(teamList, members) {
+function guildTeamStats(teamList, members, orderBy) {
+  let gameData = await (await ).json();
+  statCalculator.setGameData( gameData );
+  console.log('calculator set');
 
   const promises = [];
-  var result = [];
+
+  promises.push(fetch('https://swgoh-stat-calc.glitch.me/gameData.json').json());
   promises.push(textHelper.findAbbreviated(teamList));
   promises.push(swapi.playersInfo(members));
     
   return Promise.all(promises)
   .then(promiseResults => {
-    promiseResults[1].forEach(member => {
+    let gameData = promiseResults[0];
+    let ids = promiseResults[1];
+    let members = promiseResults[2];
+
+    var result = [];
+    members.forEach(member => {
       var orderedRoster = new Array(5);
       var trimmedRoster = [];
+      var totalGp = 0;
 
-      var filteredRoster = member.roster.filter(unit => promiseResults[0].includes(unit.defId)).map(e => {        
+      var filteredRoster = member.roster.filter(unit => ids.includes(unit.defId)).map(e => {        
         var i = promiseResults[0].indexOf(e.defId);
         orderedRoster[i] = e;
+        totalGp += e.gp;
       });
       
       orderedRoster.map(o => {
@@ -37,28 +49,33 @@ function guildTeamStats(teamList, members) {
           trimmedRoster.push(o);
         }
       });
-
+      
       result.push({
         "allyCode": member.allyCode,
         "name": member.name,
-        "units": trimmedRoster
+        "units": trimmedRoster,
+        "totalGp": totalGp
       });
-
     });
-    return result;
+
+    if (orderBy == "p") {
+      return result.sort((f, s) => s.totalGp - f.totalGp);
+    } else {
+      return result.sort((f, s) => f.name.toUpperCase() - s.name.toUpperCase() ? -1 : 1);
+    }
   })
   .catch(err => { throw err; });
 }
 
 function guildTeamImage(paths) {
   // scalato dello 0.75
-  const HEIGHT = paths.length * 116.25;
+  const HEIGHT = paths.length * 124;
   const WIDTH = 480;
 
   return new Promise(async (resolve, reject) => {
     var background = await Jimp.read(WIDTH, HEIGHT, 0x00ffffff);
     for (let i = 0; i < paths.length; i++) {
-        background.blit((await Jimp.read(paths[i])), 0, i * 155);
+        background.blit((await Jimp.read(paths[i])), 0, i * 124);
     };
 
     var timestamp = new Date().getTime();
@@ -72,19 +89,19 @@ const textHelper = new TextHelper();
 const processor = new ImageProcessor();
 var startTime = (new Date()).getTime();
 
-console.log('Inizio operazioni: richiamo i membri della gilda');
+console.log('Inizio delle operazioni.');
 swapi.guildMembers('914315138')
 .then(members => {
-  console.log('Fin qui impiegati ' + String((new Date()).getTime() - startTime) + 'ms. Inizio a prendere le stat dei personaggi per ogni membro della gilda');
+  // console.log('Fin qui impiegati ' + String((new Date()).getTime() - startTime) + 'ms. Inizio a prendere le stat dei personaggi per ogni membro della gilda');
   var players = members.map(m => m.allyCode);
-  var teamList = ['shaak','rex','echo','5','arc'];
-  //var teamList = ['CLS','HAN','CIUBE','C3PO','3POCIUBE'];
+  var teamList = ['GBA','SUNFAC','SOLDATOGEO','POGGLE','SPIA'];
+  //var teamList = ['JML','SLKR','REY','SEE'];
   //var teamList = ['DR','BSF','HK','PREDATORE','MALAK'];
-  return guildTeamStats(teamList, players);
+  return guildTeamStats(teamList, players, 'p');
 })
 .then(guildMembers => {
   membersWithPortraits = [];
-  console.log('Fin qui impiegati ' + String((new Date()).getTime() - startTime) + 'ms. Inizio a preparare le promise dei ritratti di team per ciascun membro della gilda');
+  // console.log('Fin qui impiegati ' + String((new Date()).getTime() - startTime) + 'ms. Inizio a preparare le promise dei ritratti di team per ciascun membro della gilda');
   guildMembers.forEach(member => {
     membersWithPortraits.push({
       "allyCode": member.allyCode,
@@ -96,7 +113,7 @@ swapi.guildMembers('914315138')
   return membersWithPortraits;
 })
 .then(guildMember => {
-  console.log('Fin qui impiegati ' + String((new Date()).getTime() - startTime) + 'ms. Aggrego le promise dei ritratti di team per ciascun membro della gilda');
+  // console.log('Fin qui impiegati ' + String((new Date()).getTime() - startTime) + 'ms. Aggrego le promise dei ritratti di team per ciascun membro della gilda');
   promises = [];
   guildMember.forEach(m => {
     promises.push(m.portrait);
@@ -105,11 +122,14 @@ swapi.guildMembers('914315138')
   return promises;
 })
 .then(promises => {
-  console.log('Fin qui impiegati ' + String((new Date()).getTime() - startTime) + 'ms. Inizio a risolvere le promise');
+  // console.log('Fin qui impiegati ' + String((new Date()).getTime() - startTime) + 'ms. Inizio a risolvere le promise');
   return Promise.all(promises)
 })
 .then(paths => {
   return guildTeamImage(paths);
 })
-.then(finalPath => console.log(finalPath))
+.then(finalPath => {
+  console.log(finalPath);
+  console.log('Operazioni concluse in ' + String((new Date()).getTime() - startTime) + ' ms.');
+})
 .catch(e => console.log(e));
